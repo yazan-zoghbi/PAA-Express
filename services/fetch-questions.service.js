e271fbedf3e32fa29172f95061689c1808a41f37
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-05-04 10:52:07 +0300

    [ADD] questions & query validations added to main fetch method

--- services/fetch-questions.service.js
+++ services/fetch-questions.service.js
@@ -1,14 +1,22 @@
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
+const validateQuery = require("../utils/validate-query");
+const validateQuestions = require("../utils/validate-questions");
+
 module.exports = {
   fetch: async (query) => {
-    const trimmedQuery = query.trim().replace(/\s/g, "+");
+    if (!validateQuery.check(query)) {
+      return { error: "Invalid query" };
+    }
+
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
-    await page.goto(`https://www.google.com/search?q=${trimmedQuery}`);
+    await page.goto(
+      `https://www.google.com/search?q=${validateQuery.trim(query)}`
+    );
 
     const html = await page.content();
     await browser.close();
@@ -19,6 +27,12 @@ module.exports = {
     $(".iDjcJe.IX9Lgd.wwB5gf > span").each((i, el) => {
       questions[i] = $(el).text();
     });
+
+    if (!validateQuestions.check(questions)) {
+      return {
+        error: "No related questions found, try to use more specific query!",
+      };
+    }
 
     for (let i = 0; i < questions.length; i++) {
       results.push({
